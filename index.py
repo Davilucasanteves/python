0f11ad5e5ca26a7c9226f502ea9c322b79d65cf3
Jaguar <24680000za@gmail.com>
2023-07-30 13:37:12 -0300

    3

---
+++ index.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+navegador = webdriver.Chrome()
+navegador.get("https://web.whatsapp.com")
+
+#executable_path=r'./chromedriver-linux64' kkkk
