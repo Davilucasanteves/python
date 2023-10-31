0cd3a5abefabe91196337ceae937c1c60501ebf5
Daví Lucas <127633524+Davilucasanteves@users.noreply.github.com>
2023-10-31 16:47:13 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+#EF1_Daví Lucas.py
 
-navegador = webdriver.Chrome()
-navegador.get("https://web.whatsapp.com")
-
-#executable_path=r'./chromedriver-linux64' kkkk
+def velcorpo (raio, ang, intemp):
+    return raio*ang/intemp
+    
+def acentr (velocidade, raio):
+    return velocidade*velocidade/raio
+    
+while(True):
+    print("----Velocidade e aceleração de um corpo em MRU----\n")
+    dang=float(input("Digite o deslocamento angular(rad):  "))
+    r=float(input("Digite o raio da trajetória(m):  "))
+    temp=float(input("Digite o intervalo de tempo(s):  "))
+    vel=velcorpo(r,dang,temp)
+    print("A velocidade do corpo é %g m/s"% vel)
+    print("A aceleração centrípeta do corpo é %g m/s²"% acentr(vel, r))
+    op=input("\nDigite [Enter] para repetir ou outra tecla pra sair: \n")
+    if op!="":
+        break
